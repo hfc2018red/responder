1e585d67495b30ae211d6eed986185cd35b9e0c3
Susannah Lowe <lowesusannah@gmail.com>
2018-10-20 01:56:13 -0700

    reinstall npm locally

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Don't Learn React
           </a>
         </header>
       </div>
