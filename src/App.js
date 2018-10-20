e8454a7fbc290315e1e434268aa51494ef90968c
Susannah Lowe <lowesusannah@gmail.com>
2018-10-19 20:57:56 -0700

    install Ant Design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DatePicker } from 'antd';
+
 
 class App extends Component {
   render() {
@@ -9,7 +11,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code>
+            <DatePicker />
           </p>
           <a
             className="App-link"
